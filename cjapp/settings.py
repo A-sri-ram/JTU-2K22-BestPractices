b3cfa28db8a3fa71efb0053f0ea248cc20d76da1
A-sri-ram <arumulla.sriram@trilogy.com>
2022-05-23 12:41:07 +0000

    Configured few more environment variables

--- cjapp/settings.py
+++ cjapp/settings.py
@@ -131,7 +131,7 @@ WSGI_APPLICATION: str = 'cjapp.wsgi.application'
 DATABASES: Dict[str, dict[str: str]] = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': env("DATABASE"),
     }
 }
 
